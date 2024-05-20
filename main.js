30123af7e8074f9b1cfef100807c2d05f5dbc0cb
dnyaneshwarmagar <dnyaneshwarmagar276@gmail.com>
2024-05-21 03:20:11 +0530

    done4

--- main.js
+++ main.js
@@ -6,17 +6,8 @@ const server = http.createServer((req, res) => {
     // Get the file path from the command line arguments
     const filePath = process.argv[2];
 
-
-
-    // Read the file asynchronously
-    fs.readFile(filePath, 'utf8', (err, data) => {
-
-
-            // If the file is read successfully and has the correct content, send a 200 OK response with the file contents
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end(data);
-    })
-
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end("Hello, World!\n");
 });
 
 // Start the server and listen on port 3000
