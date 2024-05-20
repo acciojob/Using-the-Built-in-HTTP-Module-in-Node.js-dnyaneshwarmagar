0ddc99f0a955244133fe2f81e981b19a8af5de21
dnyaneshwarmagar <dnyaneshwarmagar276@gmail.com>
2024-05-21 03:17:03 +0530

    done2

--- main.js
+++ main.js
@@ -6,9 +6,21 @@ const server = http.createServer((req, res) => {
     // Get the file path from the command line arguments
     const filePath = process.argv[2];
 
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end("Hello, World!\n");
+    // If no file path is provided, send a 400 Bad Request response
+    if (!filePath) {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        res.end('Error: No file path provided. Please specify a file path as a command-line argument.');
+        return;
+    }
 
+    // Read the file asynchronously
+    fs.readFile(filePath, 'utf8', (err, data) => {
+
+
+            // If the file is read successfully and has the correct content, send a 200 OK response with the file contents
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end(data);
+    })
 
 });
 
